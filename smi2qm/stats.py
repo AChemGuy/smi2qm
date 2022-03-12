7cefffdff8023ffbbce689e898b7b835115ac17f
asimnajibi <90228932+asimnajibi@users.noreply.github.com>
2022-03-13 09:50:22 +1100

    Update stats.py

--- smi2qm/stats.py
+++ smi2qm/stats.py
@@ -19,11 +19,15 @@ collection = db.test
 moldoc = collection.find_one({'smi': <'string'>}, {"opt xyz" : 1, "_id" : 0})
 with open('optstruc.xyz', 'w') as w:
  w.write(moldoc["opt xyz"])
-atoms = np.genfromtxt('optstruc.xyz', dtype=str, usecols=0)
-coords = np.genfromtxt('optstruc.xyz', dtype=float, usecols=(1,2,3))
+npatoms = np.genfromtxt('optstruc.xyz', dtype=str, usecols=0)
+npcoords = np.genfromtxt('optstruc.xyz', dtype=float, usecols=(1,2,3))
 os.remove('optstruc.xyz')
 
-#min and max no. of atoms, av.
-natomsdoc = collection.find({}, {"no. of atoms": 1, "_id" : 0})
-#use distinct to get list of values only
+#min and max no. of atoms, av. size of molecule in data set
+natomslist = collection.distinct("no. of atoms")
+npnatoms = np.array(natomslist)
+smallest = npatoms.min()
+largest = npatoms.max()
+avsize = npatoms.mean()
+#more useful measures to be added
 
