70bcff15a23d3288e4ee77b25bcf92ef0bf3c629
asimnajibi <90228932+asimnajibi@users.noreply.github.com>
2022-03-13 12:51:41 +1100

    Update stats.py

--- smi2qm/stats.py
+++ smi2qm/stats.py
@@ -2,32 +2,30 @@
 In progress...
 ######
 
-#need to incorporate into docker image usage
-
 import numpy as np
 import os
-import pymongo
-from pymongo import MongoClient
+from pymongo.cursor import Cursor
 
-#MongoDB client, db name and collection name
-#need to get user-specified names, as done for main.py 
-client=MongoClient()
-db = client.smi2qm
-collection = db.test
+class NPSTATS():
 
 #for individual molecules, atoms and coords have corresponding numpy array rows
-moldoc = collection.find_one({'smi': <'string'>}, {"opt xyz" : 1, "_id" : 0})
-with open('optstruc.xyz', 'w') as w:
- w.write(moldoc["opt xyz"])
-npatoms = np.genfromtxt('optstruc.xyz', dtype=str, usecols=0)
-npcoords = np.genfromtxt('optstruc.xyz', dtype=float, usecols=(1,2,3))
-os.remove('optstruc.xyz')
+ def atomcoords():
+  moldoc = collection.find_one({'smi': <'string'>}, {"opt xyz" : 1, "_id" : 0})
+  with open('optstruc.xyz', 'w') as w:
+   w.write(moldoc["opt xyz"])
+  npatoms = np.genfromtxt('optstruc.xyz', dtype=str, usecols=0)
+  npcoords = np.genfromtxt('optstruc.xyz', dtype=float, usecols=(1,2,3))
+  os.remove('optstruc.xyz')
+  return "atoms: "+npatoms, "coordinates: "+npcoords
 
 #min and max no. of atoms, av. size of molecule in data set
-natomslist = collection.distinct("no. of atoms")
-npnatoms = np.array(natomslist)
-smallest = npatoms.min()
-largest = npatoms.max()
-avsize = npatoms.mean()
+ def npnatoms():
+  natomslist = collection.distinct("no. of atoms")
+  npnatoms = np.array(natomslist)
+  smallest = npatoms.min()
+  largest = npatoms.max()
+  avsize = npatoms.mean()
+  return "smallest molecule: "+smallest+" atoms", "largest molecule: "+largest+" atoms", "average size of molecule: "+avsize+" atoms"
+ 
 #more useful measures to be added
 
